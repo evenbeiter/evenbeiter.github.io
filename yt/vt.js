e593db58e002ac768c1ddfe8f51367fc028ae1e5
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-16 20:27:25 +0800

    Update vt.js

--- yt/vt.js
+++ yt/vt.js
@@ -1,12 +1,46 @@
 
-let url=window.dataLayer[0]["gtm.videoUrl"];
-let captionLines=window.__NUXT__.data[0].video.captionLines;
+let data=window.__NUXT__.data[0];
+let title=data.video.title;
+let id=data.video.youtubeId;
+let captionLines=data.video.captionLines;
 var startAt=0;
 var originalText='';
 var translatedText='';
-for (var i=0;i<captionLines.length;i++){
-  startAt=captionLines[i].startAt;
-  originalText=captionLines[i].originalText.text;
-  translatedText=captionLines[i].translatedText.text;
-  console.log(startAt+originalText+translatedText)
+var txt='';
+if (data.analytics.properties.video_translated==true){
+  for (var i=0;i<captionLines.length;i++){
+    startAt=captionLines[i].startAt;
+    originalText=captionLines[i].originalText.text;
+    translatedText=captionLines[i].translatedText.text;
+    txt=txt+timeMark(startAt)+originalText+'<br>'+translatedText+'\r\n';
+  }
+} else {
+    for (var i=0;i<captionLines.length;i++){
+    startAt=captionLines[i].startAt;
+    originalText=captionLines[i].originalText.text;
+    txt=txt+timeMark(startAt)+originalText+'\r\n';
+  }
 }
+
+function timeMark(sec){
+    let mm=Math.floor(sec/60);
+    let ss=sec-mm*60;
+    ss=ss.toFixed(2);
+    return '['+[("0"+mm).slice(-2),("0"+ss).slice(-5)].join(':')+']';
+}
+
+alert('["'+title+'","'+id+'"],');
+
+var txtData = new Blob([txt], { type: 'data:text/csv;charset=utf-8' });
+var txtUrl = URL.createObjectURL(txtData); 
+let link = document.createElement('a');
+link.href = txtUrl;
+link.target = '_blank';
+link.download = title+'.txt';
+link.click();
+
+
+
+
+
+
