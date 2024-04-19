f9a3f06c89c18f23effb7dedaa4103a6aa39c847
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-04-19 09:44:21 +0800

    Update soeasyedu-cnn.js

--- yt/soeasyedu-cnn.js
+++ yt/soeasyedu-cnn.js
@@ -77,7 +77,6 @@ document.getElementById('lines').addEventListener('click', function (e) {
   }
 });
 
-document.close();
 </script>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
@@ -87,5 +86,5 @@ document.close();
 </html>`;
 
 win=window.open('','','');void(win.document.write(code0+txt+code1));
-
+document.close();
 })();
