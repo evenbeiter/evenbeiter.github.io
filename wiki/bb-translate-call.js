81809d616d6462d99fc93f41404c0e59db9ce7b0
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-06-28 07:51:37 +0800

    Update bb-translate-call.js

--- wiki/bb-translate-call.js
+++ wiki/bb-translate-call.js
@@ -3,11 +3,27 @@ javascript:(async()=>{
 const code0=`
 <!doctype html>
 <html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>TRANSLATE</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>TRANSLATE</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+  <style>
+    body {background-color:#F4ECD8;color:#5B4636;font-size:1.3rem}
+    .content{display:none;cursor:pointer}
+    .sepia{background-color:#F4ECD8;color:#5B4636}
+    .sepia-contrast{background-color:#3B2D20;color:#F4ECD8}
+    h1,h2,h3,h4,h5,h6,.title{font-size:1.3rem;color:#3B2D20 !important;font-weight:bold}
+    a{word-wrap:break-word;white-space:normal;overflow-wrap:break-word;word-break:break-word}
+    h1 a {color: navy; font-weight: bold; text-decoration:none;cursor:pointer}
+    figcaption{font-size:1rem}
+    img{display:block !important;margin:auto;width:100%;height:auto}
+    @media (min-width:768px){
+      @font-face{font-family:"Microsoft JhengHei Bold";src:local("Microsoft JhengHei Bold");unicode-range:U+4E00-9FFF}
+      body {background-color:#F4ECD8;color:#5B4636;font-size:1.2rem;font-family:arial,'Microsoft JhengHei Bold', sans-serif !important}
+      h1,h2,h3,h4,h5,h6,.title{font-size:1.2rem;color:#3B2D20 !important;font-weight:normal}
+    }
+  </style>
 </head>
 <body>
 
