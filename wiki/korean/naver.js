a1def34c7817692ee01498f98bf2bd4cc97fe236
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-11-11 20:09:15 +0800

    Update naver.js

--- wiki/korean/naver.js
+++ wiki/korean/naver.js
@@ -1,9 +1,9 @@
 
 javascript: (async()=>{
 var li=[...document.getElementsByClassName('_txt_origin_wrap'),...document.getElementsByClassName('example_lst')[0].children];
-var txt='逐字詳細解析以下句子的單詞活用、文法和句型。把每個韓文句子的每段解析都寫成用 \<p\> 標籤包裹的HTML格式，並以標準字體顯示。最後再把所有的HTML寫成JSON格式，每一個韓文句子為JSON的一個項目。';
+var txt='逐字詳細解析以下句子的單詞活用、文法和句型。把每個韓文句子的每段解析都寫成用p標籤包裹的HTML格式，並以標準字體顯示。最後再把所有的HTML寫成JSON格式，每一個韓文句子為JSON的一個項目。';
 for (var i=0;i<li.length;i++){
-    txt+=`${i+1}. ${li[i].innerText.slice(0,li[i].innerText.indexOf('\n'))} `;
+    txt+=`${i+1}. ${li[i].innerText.slice(0,li[i].innerText.indexOf('\n'))} <br>`;
 }
 win=window.open('','','');void(win.document.write(txt));
 })();
