49a123569211e52d291d44d353753878c1ffb49c
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-11-13 09:10:11 +0800

    Update naver-page.js

--- wiki/korean/naver-page.js
+++ wiki/korean/naver-page.js
@@ -28,8 +28,8 @@ var res=await fetch('https://gateway.dict.naver.com/krdict/kr/kozh/today/'+date+
 var str=await res.text();
 var data=JSON.parse(str.slice(21,-1)).data;  
 
+try {
 var res=await fetch('https://evenbeiter.github.io/wiki/korean/'+date+'.json');
-if (res.status==200){
 document.getElementById('list').style.display='block';
 document.getElementById('gpt').style.display='none';
 var gpt=await res.json();
@@ -53,13 +53,14 @@ for (let t of data.entrys){
   vocab+=\`<p onclick="playAudio('\${t.pron_file_url}')">\${t.orgnc_entry_name}<br>\${t.mean}</p><hr>\`;
 }
 document.getElementById('vocab').innerHTML=vocab;
-}else{
+}catch{console.log('here');
 document.getElementById('list').style.display='none';
 document.getElementById('gpt').style.display='block';
 var txt='<p>逐字詳細解析以下句子包括單詞活用、文法和句型三大段說明。並把每段說明的標題(單詞活用、文法、句型)和說明的內文都寫成用p標籤包裹的HTML格式，標題和各單詞及文法和句型的說明都自成一個p標籤，每個單詞的活用說明也都是各自獨立的p標籤；標題用粗體、內文用標準字體顯示。最後再把所有的HTML寫成JSON格式，每一個韓文句子的解析整合為一個字串(裡面應該會包含多個p標籤)，成為JSON的一個項目。JSON的格式為\[字串,字串…\］。</p><br>';
 var n=1;
-for (let s of data.sentences){txt+=\`<p>\${n++}. \${s.orgnc_sentence}</p>\`}
-for (let e of data.studys[0].examples){txt+=\`<p>\${n++}. \${e.origin_example}</p>\`}
+for (let s of data.sentences){txt+=\`<p>\${n++}. \${s.orgnc_sentence}</p>\`};
+for (let e of data.studys[0].examples){txt+=\`<p>\${n++}. \${e.origin_example}</p>\`};
+document.getElementById('gpt').innerHTML=txt;
 }
 }
 
