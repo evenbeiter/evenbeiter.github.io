2b336bb22c31d903e2b2bee186e77f33492a9f06
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2024-11-13 08:23:28 +0800

    Update naver.js

--- wiki/korean/naver.js
+++ wiki/korean/naver.js
@@ -1,7 +1,7 @@
 
 javascript: (async()=>{
 var date=window.location.href.slice(53,61);
-var txt='<p>逐字詳細解析以下句子包括單詞活用、文法和句型三大段說明。並把每段說明的標題(單詞活用、文法、句型)和說明的內文都寫成用p標籤包裹的HTML格式，標題和各單詞及文法和句型的說明都自成一個p標籤，每個單詞的活用說明也都是各自獨立的p標籤；標題用粗體、內文用標準字體顯示。最後再把所有的HTML寫成JSON格式，每一個韓文句子的解析整合為一個字串(裡面應該會包含多個p標籤)，成為JSON的一個項目。JSON的格式為[字串,字串…］。</p><br>';
+var txt='<div style="font-size:12px"><p>逐字詳細解析以下句子包括單詞活用、文法和句型三大段說明。並把每段說明的標題(單詞活用、文法、句型)和說明的內文都寫成用p標籤包裹的HTML格式，標題和各單詞及文法和句型的說明都自成一個p標籤，每個單詞的活用說明也都是各自獨立的p標籤；標題用粗體、內文用標準字體顯示。最後再把所有的HTML寫成JSON格式，每一個韓文句子的解析整合為一個字串(裡面應該會包含多個p標籤)，成為JSON的一個項目。JSON的格式為[字串,字串…］。</p><br>';
 var res=await fetch('https://gateway.dict.naver.com/krdict/kr/kozh/today/'+date+'/conversation.dict?callback=angular.callbacks._0');
 var str=await res.text();
 var data=JSON.parse(str.slice(21,-1)).data;
@@ -12,6 +12,7 @@ for (let s of data.sentences){
 for (let e of data.studys[0].examples){
   txt+=`<p>${n++}. ${e.origin_example}</p>`
 }
+txt+='</div>';
 win=window.open('','','');void(win.document.write(txt));win.document.close();
 })();
 
