9dbee8d19aa6f643b23d389765b565eb8110bf6d
evenbeiter <45744312+evenbeiter@users.noreply.github.com>
2025-04-29 21:13:47 +0800

    Update newsbkml.js

--- js/newsbkml.js
+++ js/newsbkml.js
@@ -5,29 +5,8 @@ javascript:(function(){
   const injectScript = () => {
     const script = document.createElement('script');
     script.src = externalScriptUrl;
-    script.onload = () => console.log('Script loaded.');
-    document.body.appendChild(script);
-  };
-
-  if (location.href !== targetUrl) {
-    window.location.href = targetUrl;
-    window.addEventListener('load', injectScript, false);
-  } else {
-    injectScript();
-  }
-})();
-
-
-
-javascript:(function(){
-  const targetUrl = "https://example.com";
-  const externalScriptUrl = "https://yourdomain.com/logic.js";
-
-  const injectScript = () => {
-    const script = document.createElement('script');
-    script.src = externalScriptUrl;
-    script.onload = () => console.log('[Bookmarklet] Script loaded.');
-    script.onerror = () => console.error('[Bookmarklet] Script failed to load.');
+    script.onload = () => alert('[Bookmarklet] Script loaded.');
+    script.onerror = () => alert('[Bookmarklet] Script failed to load.');
     document.body.appendChild(script);
   };
 
@@ -41,7 +20,6 @@ javascript:(function(){
 
   if (location.href !== targetUrl) {
     window.location.href = targetUrl;
-    // 儲存狀態以便 reload 後知道該注入腳本
     sessionStorage.setItem('__inject_script__', 'true');
   } else if (sessionStorage.getItem('__inject_script__')) {
     sessionStorage.removeItem('__inject_script__');
